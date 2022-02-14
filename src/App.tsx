345c37dab2e98c30247961c7e2abb65e300b13d3
cdoninja <christine.do@gmail.com>
2022-02-13 17:05:03 -0800

    Remove import of unused var

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ import { InfoModal } from './components/modals/InfoModal'
 import { StatsModal } from './components/modals/StatsModal'
 import { SettingsModal } from './components/modals/SettingsModal'
 import {
-  GAME_TITLE,
+  // GAME_TITLE,
   WIN_MESSAGES,
   GAME_COPIED_MESSAGE,
   ABOUT_GAME_MESSAGE,
